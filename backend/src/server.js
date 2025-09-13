134f8a48ffb326060e22971c0b551b672e732645
Phan Đức Việt Anh <kinosuke@openreach.tech>
2025-09-13 10:11:44 +0700

    Setup for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import express from 'express'
 import dotenv from 'dotenv'
 
@@ -7,9 +9,18 @@ import messageRoutes from './routes/message.route.js'
 dotenv.config()
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist','index.html'))
+  })
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`)
