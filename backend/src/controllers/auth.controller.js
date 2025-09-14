e3ae6f370a9ce9e7a7ab6fa869bd95c6558dc142
Phan Đức Việt Anh <kinosuke@openreach.tech>
2025-09-14 15:10:51 +0700

    Implement login and logout feature for backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,3 +67,53 @@ export const signup = async (req, res) => {
       .json({ message: 'Internal server error' })
   }
 }
+
+export const login = async (req, res) => {
+  const {
+    email,
+    password,
+  } = req.body
+
+  try {
+    if (!email || !password) {
+      return res.status(400)
+        .json({ message: 'All fields are required' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400)
+        .json({ message: 'Invalid email or password' })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400)
+        .json({ message: 'Invalid email or password' })
+    }
+
+    generateToken(user._id, res)
+
+    return res.status(200)
+      .json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      })
+  } catch (error) {
+    return res.status(500)
+      .json({ message: 'Internal server error' })
+  }
+}
+
+export const logout = (_, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    maxAge: 0,
+  })
+
+  return res.status(200)
+    .json({ message: 'Logged out successfully' })
+}
