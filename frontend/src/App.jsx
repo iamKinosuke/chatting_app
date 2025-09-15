bb2cddf120fe0e45a04e0555749c2308fbbabf32
Phan Đức Việt Anh <kinosuke@openreach.tech>
2025-09-15 22:09:32 +0700

    Implement signup for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,40 @@
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
+
+import { useAuthStore } from './store/useAuthStore.js'
+
+import { useEffect } from 'react'
+
+import { Toaster } from 'react-hot-toast'
+
+import PageLoader from './components/PageLoader'
 
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import ChatPage from './pages/ChatPage'
 
 const App = () => {
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+  console.log(authUser)
+
+  if (isCheckingAuth) {
+    return <PageLoader />
+  }
+
   return (
     <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
       </Routes>
+      <Toaster />
     </div>
   )
 }
